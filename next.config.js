bf1d5da2ebacda17e35f6f8287a6b43cbe20d797
Jibéo <jibeo@macbook-pro.home>
2023-12-06 16:28:22 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -2,16 +2,17 @@ if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
   throw new Error(`
     Please provide a valid WordPress instance URL.
     Add to your environment variables WORDPRESS_API_URL.
-  `)
+  `);
 }
 
 const { protocol, hostname, port, pathname } = new URL(
   process.env.WORDPRESS_API_URL
-)
+);
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
+    domains: ["secure.gravatar.com", "jbolsommer.fr"],
     remotePatterns: [
       {
         protocol: protocol.slice(0, -1),
@@ -21,4 +22,4 @@ module.exports = {
       },
     ],
   },
-}
+};
